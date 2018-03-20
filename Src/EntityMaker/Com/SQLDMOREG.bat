6fcea8d6cc542dc871a741012b29b9e14b9ae22c
leoualy <liuyuanaaron@icloud.com>
2018-03-20 10:54:46 +0800

    添加 SQLDMO.DLL 组件的批处理

---
+++ Src/EntityMaker/Com/SQLDMOREG.bat
@@ -0,0 +1,7 @@
+﻿:注册SQLDMO.dll 的批处理
+
+: 将当前目录下的SQLDMO.dll 复制到 C:\ProgramFiles\Microsoft SQL Server\80\Tools\Binn下
+copy .\SQLDMO.DLL  C:\ProgramFiles\Microsoft SQL Server\80\Tools\Binn\SQLDMO.DLL
+
+: 注册该Com组件
+regsvr32"C:\ProgramFiles\Microsoft SQL Server\80\Tools\Binn\SQLDMO.DLL"
